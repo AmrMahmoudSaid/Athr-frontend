6241aa883b41f4fe540c9da8987c0ea56e9ffe18
amr mahmoud <amrmahmoud1900@gmail.com>
2026-06-27 19:27:56 +0300

    add all services

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-const BASE = 'http://localhost:3000/api/v1';
+const BASE = environment.apiUrl;
 
 export interface RegisterPayload {
   fullNameArabic: string;
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://athr-backend-app-pk7fk.ondigitalocean.app/api/v1',
+};
